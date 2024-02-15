447666908a42f9348cbdac0eb18fcdabb2844533
JohnB08 <johnb08.kodehode@gmail.com>
2024-02-15 09:41:06 +0000

    login and user creation input fields.

--- client/Docker-Vite-Test/src/App.tsx
+++ client/Docker-Vite-Test/src/App.tsx
@@ -1,73 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { ChangeEvent, useState } from 'react'
 import './App.css'
 import { useFetch, mainUrl } from './Utils/Hooks/Fetch'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [url, setUrl] = useState<string>(mainUrl)
   const [options, setOptions] = useState<RequestInit | null>(null)
   const [data, error] = useFetch(url, options)
+  const [name, setName] = useState<string>("")
+  const [pw, setPw] = useState<string>("")
 
 
   const testUserServer = () =>{
+    if (name.length === 0 || pw.length === 0) return
     const serverUrl = mainUrl + "/create"
     setUrl(serverUrl)
     const requestHeaders: RequestInit = {
       method: "POST",
       headers: {"Content-Type": "application/json"},
       body:JSON.stringify({
-        username: "Bjarne",
-        password: "123456"
+        username: name,
+        password: pw
       })
     }
     setOptions(requestHeaders)
   }
 
   const testUserLogin = () =>{
+    if (name.length === 0 || pw.length === 0) return
     const serverUrl = mainUrl + "/login"
     setUrl(serverUrl)
     const requestHeaders: RequestInit = {
       method: "POST",
       headers: {"Content-Type": "application/json"},
       body: JSON.stringify({
-        username: "Bjarne",
-        password: "123456"
+        username: name,
+        password: pw
       })
     }
     setOptions(requestHeaders)
   }
   console.log(data,error)
 
+  const updateName = (event: ChangeEvent<HTMLInputElement>) =>{
+    setName(event.target.value)
+  }
+  console.log(name)
+  const updatePw = (event: ChangeEvent<HTMLInputElement>) =>{
+    setPw(event.target.value)
+  }
+  console.log(pw)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='mainContainer'>
+      <div className='inputContainer'>
+        <label htmlFor="nameInput">Input Name <input type="text" name="nameInput" id="nameInput" defaultValue="" onChange={updateName}/> </label>
+        <label htmlFor="pwInput">Input Password <input type="text" name="pwInput" id="pwInput" defaultValue="" onChange={updatePw}/> </label>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className='buttonContainer'>
         <button onClick={()=>testUserServer()}>
           Test Create User
         </button>
         <button onClick={()=>testUserLogin()}>
           Test Login User
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
